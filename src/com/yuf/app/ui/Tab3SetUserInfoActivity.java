f4831d16981a10bfcd09b2d12e2cfc36c2940fc5
liowmark <liowmark@163.com>
2014-09-04 00:38:51 +0800

    加入用户设置界面 mark version 0.01 2014.9.3

---
+++ src/com/yuf/app/ui/Tab3SetUserInfoActivity.java
@@ -0,0 +1,43 @@
+package com.yuf.app.ui;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.yuf.app.MyApplication;
+import com.yuf.app.http.extend.BitmapCache;
+
+import android.R.mipmap;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class Tab3SetUserInfoActivity extends Activity {
+private ImageView backImageView;
+private NetworkImageView profileImageView;
+private ImageLoader mImageLoader;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		mImageLoader=new ImageLoader(MyApplication.requestQueue, new BitmapCache());
+		setContentView(R.layout.tab3_setuserinfo_activity);
+		profileImageView=(NetworkImageView)findViewById(R.id.tab3_setuserinfo_profile);
+		profileImageView.setDefaultImageResId(R.drawable.no_pic);
+		profileImageView.setErrorImageResId(R.drawable.no_pic);
+		profileImageView.setImageUrl("http://www.baidu.com", mImageLoader);
+		backImageView=(ImageView)findViewById(R.id.tab3_setUserInfo_back_imageview);
+		backImageView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onBackPressed();
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+	
+
+}
