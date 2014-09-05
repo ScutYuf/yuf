3e19eb229998712aef160912267020e5be7ee049
zm <2263683418@qq.com>
2014-09-05 20:47:02 +0800

    test

--- src/com/yuf/app/db/Order.java
+++ src/com/yuf/app/db/Order.java
@@ -22,14 +22,17 @@ public class Order {
 	public static final String ORDERPAYMETHOD = "orderPaymethod";
 	public static final String ORDERAMOUNT = "orderAmount";
 	public static final String ORDERDISHID = "dishId";
-
+	public static final String ORDERIMAGE = "orderImage";
+	public static final String ORDERNAME = "orderName";
+	
 	public int userId;
 	public int dishId;
 	public double orderPrice;
 	public String orderTime;
 	public String orderPaymethod;
 	public int orderAmount;
-
+	public String orderImage;
+	public String orderName;
 	public JSONObject toJsonObject() {
 		JSONObject object = new JSONObject();
 		try {
@@ -39,6 +42,8 @@ public class Order {
 			object.put("orderTime", orderTime);
 			object.put("orderPaymethod", orderPaymethod);
 			object.put("orderAmount",orderAmount);
+			object.put("orderImage",orderImage);
+			object.put("orderName",orderName);
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -57,6 +62,8 @@ public class Order {
            values.put(Order.ORDERPAYMETHOD, orderPaymethod);  
            values.put(Order.ORDERAMOUNT, orderAmount);  
            values.put(Order.ORDERDISHID, dishId);  
+           values.put(Order.ORDERIMAGE, orderImage); 
+           values.put(Order.ORDERNAME, orderName);  
            Uri result = contentResolver.insert(url, values); 
            if (ContentUris.parseId(result)>0) {  
                Log.d("liow","添加成功！");        
@@ -67,7 +74,7 @@ public class Order {
 		ArrayList<Order> list = new ArrayList<Order>();
 		Uri url = Uri.parse("content://com.yuf.app.myprovider/order");  
         Cursor cursor = MyApplication.myapplication.getContentResolver().query(url,  
-                  new String[] { "_id", "userId", "orderPrice","orderTime","orderPaymethod","orderAmount","dishId" }, null, null, "_id");  
+                  new String[] { "_id", "userId", "orderPrice","orderTime","orderPaymethod","orderAmount","dishId","orderImage","orderName" }, null, null, "_id");  
         while (cursor.moveToNext()) {  
         	Order order = new Order();
         	order.userId = cursor.getInt(cursor.getColumnIndex("userId"));  
@@ -76,6 +83,8 @@ public class Order {
         	order.orderTime = cursor.getString(cursor.getColumnIndex("orderTime"));  
         	order.orderPaymethod = cursor.getString(cursor.getColumnIndex("orderPaymethod"));  
         	order.orderAmount = cursor.getInt(cursor.getColumnIndex("orderAmount"));  
+        	order.orderImage = cursor.getString(cursor.getColumnIndex("orderImage"));  
+        	order.orderName = cursor.getString(cursor.getColumnIndex("orderName"));  
         	list.add(order);  
         }  
         cursor.close();  
--- src/com/yuf/app/ui/Tab2WaitForPayActivity.java
+++ src/com/yuf/app/ui/Tab2WaitForPayActivity.java
@@ -1,43 +1,62 @@
 package com.yuf.app.ui;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
+
+import java.util.ArrayList;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.yuf.app.MyApplication;
+import com.yuf.app.db.Order;
+import com.yuf.app.http.extend.BitmapCache;
 
 public class Tab2WaitForPayActivity extends Activity {
 	private ImageView backImageView;
 	private Button okButton;
-	private ListView listView; 
-	public static Tab2WaitForPayActivity mActivity;
-	
-	
-	
+	private PullToRefreshListView listView; 
+	private ArrayList<Order> orderList;  
+    private MyListAdapter mAdapter;  
+	private ImageLoader mImageLoader;
+	private boolean isEnd;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		mActivity=this;
+		mImageLoader = new ImageLoader(MyApplication.requestQueue, new BitmapCache());
+		orderList=Order.readFromDb(); 
+		mAdapter = new MyListAdapter();
 		setContentView(R.layout.tab2_wait_pay);
+		listView=(PullToRefreshListView)findViewById(R.id.tab2_waitforpay_listView);
+		listView.setMode(Mode.BOTH);
+		listView.setAdapter(mAdapter);
 		
-		listView=(ListView)findViewById(R.id.tab2_waitforpay_listView);
-		SimpleAdapter adapter = new SimpleAdapter(this,getData(),R.layout.tab2_waitforpay_item,
-				new String[]{"name","head_img","account"},
-				new int[]{R.id.myfocus_name_text,R.id.myfocus_head_img,R.id.myfocus_account_text});//that is no right ,just for view
-		 listView.setAdapter(adapter);
-		
-		
-		
+		listView.setOnRefreshListener(new OnRefreshListener<ListView>() {
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+				 String label = DateUtils.formatDateTime(getApplicationContext(), System.currentTimeMillis(),  
+	                        DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);  
+				 // Update the LastUpdatedLabel  
+	             refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);  
+				// Do work to refresh the list here.
+	             refreshListView();    
+			}
+});
 		backImageView=(ImageView)findViewById(R.id.tab2_waitforpay_back_imageView);
 		backImageView.setOnClickListener(new OnClickListener() {
 			
@@ -53,37 +72,57 @@ public class Tab2WaitForPayActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent intent =new Intent(mActivity,Tab2AddressActivity.class);
+				Intent intent =new Intent(Tab2WaitForPayActivity.this,Tab2AddressActivity.class);
 				startActivity(intent);
 				// TODO Auto-generated method stub
 				
 			}
 		});
-		
-		
 	}
-	
-	private List<Map<String, Object>> getData() {
-		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+	private void refreshListView() {
+		isEnd=false;
+		listView.setMode(Mode.BOTH);
+		orderList.removeAll(orderList);
+		loadingNextPage();
+	}
 
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("name", "G1");
-		map.put("account", "google 1");
-		map.put("head_img", R.drawable.ic_launcher);
-		list.add(map);
+	private void loadingNextPage() {
+		//orderList = Order.readFromDb();
+		listView.onRefreshComplete();
+		mAdapter.notifyDataSetChanged();
+	}
+	private class MyListAdapter extends BaseAdapter{
 
-		map = new HashMap<String, Object>();
-		map.put("name", "G2");
-		map.put("account", "google 2");
-		map.put("head_img", R.drawable.ic_launcher);
-		list.add(map);
+		@Override
+		public int getCount() {
+			return orderList.size();
+		}
 
-		map = new HashMap<String, Object>();
-		map.put("name", "G3");
-		map.put("account", "google 3");
-		map.put("head_img", R.drawable.ic_launcher);
-		list.add(map);
+		@Override
+		public Object getItem(int arg0) {
+			return orderList.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return arg0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Log.d("mywork", "get");
+			if (convertView==null) {
+				convertView=Tab2WaitForPayActivity.this.getLayoutInflater().inflate(R.layout.tab2_waitforpay_item,null);
+			}
+			Order order = orderList.get(position);
+			NetworkImageView imageOrder = (NetworkImageView)convertView.findViewById(R.id.tab3_mywork_item_img);
+			//imageOrder.setImageUrl(order.orderImage, mImageLoader);
+			TextView nameOfOrder=(TextView)convertView.findViewById(R.id.tab3_mywork_item_name);
+			nameOfOrder.setText(order.orderName);
+			TextView payOfOrder=(TextView)convertView.findViewById(R.id.TextView01);
+			payOfOrder.setText(order.orderAmount+"");
+			return convertView;
+		}
 		
-		return list;
 	}
 }
