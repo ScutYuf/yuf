04bf16484817744cec178c3bd15353afc6cd5d65
liowmark <liowmark@163.com>
2014-10-12 00:51:09 +0800

    分享仿微信1.0 mark version 0.10 2014.10.12

--- src/com/yuf/app/ui/Tab1SocietyShareFragment.java
+++ src/com/yuf/app/ui/Tab1SocietyShareFragment.java
@@ -3,6 +3,7 @@ package com.yuf.app.ui;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 
+import org.apache.http.protocol.ResponseConnControl;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,6 +22,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -38,6 +40,7 @@ import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleLis
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.yuf.app.MyApplication;
+import com.yuf.app.Entity.UserInfo;
 import com.yuf.app.http.extend.BitmapCache;
 
 
@@ -217,10 +220,9 @@ private class MylistAdapter extends BaseAdapter
 		// TODO Auto-generated method stub
 		Log.d("mywork", "get");
 	    ViewHolder holder;
-		final ViewGroup tmpGroup=parent;
+	    final int index=position;
 		if (convertView==null) {
 			convertView=getActivity().getLayoutInflater().inflate(R.layout.tab1_share_list_item,null);
-			
 			//
 			holder = new ViewHolder();
 			holder.headimageView=(NetworkImageView)convertView.findViewById(R.id.tab1_share_list_item_headimg);
@@ -230,6 +232,8 @@ private class MylistAdapter extends BaseAdapter
 			holder.titleTextView=(TextView)convertView.findViewById(R.id.tab1_share_item_titile_textview);
 			holder.usernameTextView=(TextView)convertView.findViewById(R.id.tab1_share_list_item_name_textview);
 			holder.moreImageView=(ImageView)convertView.findViewById(R.id.tab1_share_list_item_more);
+			holder.commentContent=(LinearLayout)convertView.findViewById(R.id.tab1_share_list_item_comment_linearlayout);
+			holder.likeTextView=(TextView)convertView.findViewById(R.id.tab1_share_list_item_like_textview);
 			convertView.setTag(holder);
 			
 		}
@@ -252,6 +256,23 @@ private class MylistAdapter extends BaseAdapter
 					int[] location = new int[2];  
 					v.getLocationOnScreen(location);  
 					mPopupWindow.showAtLocation(v, Gravity.NO_GRAVITY, location[0]-300, location[1]); 
+					TextView likeTextView=(TextView) viewGroup.findViewById(R.id.comment_popupwindow_like);
+					likeTextView.setOnClickListener(new OnClickListener() {
+						
+						@Override
+						public void onClick(View v) {
+							// TODO Auto-generated method stub
+							try {
+								addLikeRelationship(jsonArray.getJSONObject(index).getInt("postid"));
+								jsonArray.getJSONObject(index).getJSONArray("likeUsersList").put(UserInfo.getInstance().username);
+								mAdaAdapter.notifyDataSetChanged();
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					});
+				
 				}
 				else {
 					mPopupWindow.dismiss();
@@ -278,6 +299,8 @@ private class MylistAdapter extends BaseAdapter
 			holder.timeTextView.setText(formatter.format(date));
 			holder.contentTextView.setText(jsonObject.getString("postcontent"));
 			
+			showCommentItemList(holder.commentContent, jsonObject.getJSONArray("CommentList"));
+			showLikeList(holder.likeTextView, jsonObject.getJSONArray("likeUsersList"));
 			
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
@@ -296,11 +319,93 @@ private class MylistAdapter extends BaseAdapter
 		 TextView timeTextView;
 		 TextView contentTextView;
 		 ImageView moreImageView;
-		 
+		 TextView likeTextView;
+		 LinearLayout commentContent;
 		 
 		 
 		}
 	}
+private void showCommentItemList(LinearLayout content,JSONArray array)
+{
+	content.removeAllViews();
+	for (int i = 0; i < array.length(); i++) {
+		View itemView=getActivity().getLayoutInflater().inflate(R.layout.tab1_share_list_item_comment_item, null);
+		TextView usernameTextView=(TextView)itemView.findViewById(R.id.comment_name);
+		TextView contentTextView=(TextView)itemView.findViewById(R.id.comment_content);
+		try {
+			usernameTextView.setText(array.getJSONObject(i).getString("username")+":");
+			contentTextView.setText(array.getJSONObject(i).getString("commentcontent"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		content.addView(itemView);
+	}
+}
+private void showLikeList(TextView view,JSONArray array)
+{
+	view.setText("");
+	boolean isFrist=true;
+	for (int i = 0; i < array.length(); i++) {
+		try {
+			if (isFrist) {
+				view.setText(array.getString(i));
+				isFrist=false;
+			}
+			else 
+				view.setText(view.getText()+","+array.getString(i));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+private void addLikeRelationship(int postid)
+{
+	JSONObject jsonObject=new JSONObject();
+	try {
+		jsonObject.put("userId",Integer.valueOf( UserInfo.getInstance().userid));
+		jsonObject.put("postId", postid);
+	} catch (NumberFormatException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (JSONException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	JsonObjectRequest request=new JsonObjectRequest(Method.POST, "http://110.84.129.130:8080/Yuf/like/insertLike", jsonObject, new Response.Listener<JSONObject>() {
+
+		@Override
+		public void onResponse(JSONObject response) {
+			// TODO Auto-generated method stub
+			try {
+				if (response.getString("insertLike").equals("success")) {
+					Toast.makeText(getActivity(), "点赞成功", Toast.LENGTH_SHORT).show();
+				}
+				else {
+					Toast.makeText(getActivity(), "点赞失败", Toast.LENGTH_SHORT).show();
+
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	},new Response.ErrorListener() {
+
+		@Override
+		public void onErrorResponse(VolleyError error) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
+	MyApplication.requestQueue.add(request);
+	MyApplication.requestQueue.start();
+}
 
 
 }
