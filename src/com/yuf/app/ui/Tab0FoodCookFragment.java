118de6d9ac8504ad4d65850543c70d1cbbbbb9b9
zm <2263683418@qq.com>
2014-10-11 23:50:07 +0800

    一键分享初级版 zm version0.01 2014-10-11

--- src/com/yuf/app/ui/Tab0FoodCookFragment.java
+++ src/com/yuf/app/ui/Tab0FoodCookFragment.java
@@ -29,12 +29,18 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.Platform.ShareParams;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+
 import com.android.volley.Request.Method;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.NetworkImageView;
+import com.onekeyshare.OnekeyShare;
 import com.yuf.app.MyApplication;
 import com.yuf.app.Entity.UserInfo;
 import com.yuf.app.db.Order;
@@ -127,7 +133,15 @@ public class Tab0FoodCookFragment extends Fragment{
 			}
 		});
 		collection=(TextView)view.findViewById(R.id.collection_textView);
+//一键分享
 		share=(TextView)view.findViewById(R.id.share_TextView);
+		share.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				showShare();
+			}
+		});
+//加入购物车
 		addcart=(TextView)view.findViewById(R.id.add_cart_textview);
 		addcart.setOnClickListener(new OnClickListener() {
 			
@@ -153,8 +167,36 @@ public class Tab0FoodCookFragment extends Fragment{
 		addSteps();
 		return  view;
 	}
-
-
+//一键分享
+	   private void showShare() {
+	        ShareSDK.initSDK(getActivity());
+	        OnekeyShare oks = new OnekeyShare();
+	        //关闭sso授权
+	        oks.disableSSOWhenAuthorize();
+	        // 分享时Notification的图标和文字
+	        oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+	        oks.setTitle(getString(R.string.share));// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+	        oks.setTitleUrl("http://sharesdk.cn"); // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+            // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+	        oks.setImageUrl("http://www.iyi8.com/uploadfile/2014/0506/20140506085929652.jpg");
+	        oks.setUrl("http://sharesdk.cn");// url仅在微信（包括好友和朋友圈）中使用
+	        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+	        oks.setComment("我是测试评论文本");
+	        oks.setSite(getString(R.string.app_name));// site是分享此内容的网站名称，仅在QQ空间使用
+	        oks.setSiteUrl("http://sharesdk.cn");// siteUrl是分享此内容的网站地址，仅在QQ空间使用
+	        String a ="";
+	        for (int i = 0; i <stepJsonObject.length(); i++) {
+	    		JSONObject mJsonObject;
+	    		try {mJsonObject = stepJsonObject.getJSONObject(i);
+	    			a=a+String.valueOf(mJsonObject.getInt("recipeorder"))+":"+mJsonObject.getString("recipedetail")+";";
+	    		} catch (JSONException e) {e.printStackTrace();}}
+	        try {
+	        	// text是分享文本，所有平台都需要这个字段
+		        oks.setText("#煮乜嘢#"+dishInfoJsonObject.getString("dishname")+a);
+		    } catch (JSONException e) {e.printStackTrace();}
+	        // 启动分享GUI
+	        oks.show(getActivity());
+	   }
 protected void addOrder() {
 	try {
 		
