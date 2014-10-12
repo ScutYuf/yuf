a06c92dc8dd42492c4ef219ea5f7686b71702ea8
liowmark <liowmark@163.com>
2014-10-12 14:52:11 +0800

    //

--- src/com/yuf/app/ui/Tab0MyRecipeFragment.java
+++ src/com/yuf/app/ui/Tab0MyRecipeFragment.java
@@ -43,7 +43,7 @@ import com.android.volley.Response.ErrorListener;
 @SuppressLint("ValidFragment")
 public class Tab0MyRecipeFragment extends Fragment {
 
-
+//
 	private Button detailButton;
 	private Button commentbuttoButton;
 	private Button collectionButton;
