96217b8979bb83c0e8e67c50f5c6a2070dc5ff0c
zm <2263683418@qq.com>
2014-11-09 00:53:00 +0800

    瀑布流夫tab0无法显示 zm version0.01 2014-11-9

--- src/com/yuf/app/ui/Tab0MyRecipeFragment.java
+++ src/com/yuf/app/ui/Tab0MyRecipeFragment.java
@@ -1,16 +1,22 @@
 package com.yuf.app.ui;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import me.maxwin.view.XListView;
+import me.maxwin.view.XListView.IXListViewListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.R.integer;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.AsyncTask.Status;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
@@ -20,6 +26,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,21 +34,24 @@ import android.widget.Toast;
 
 import com.android.volley.Request.Method;
 import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.NetworkImageView;
+import com.dodola.model.DuitangInfo;
+import com.dodowaterfall.Helper;
+import com.dodowaterfall.widget.ScaleImageView;
+import com.example.android.bitmapfun.util.ImageFetcher;
 import com.yuf.app.MyApplication;
 import com.yuf.app.Entity.UserInfo;
-import com.yuf.app.adapter.OutsidePagerAdapter;
 import com.yuf.app.http.extend.BitmapCache;
 import com.yuf.app.mywidget.FoodViewPage;
 import com.yuf.app.ui.indicator.CirclePageIndicator;
-import com.android.volley.Response.ErrorListener;
 
 @SuppressLint("ValidFragment")
-public class Tab0MyRecipeFragment extends Fragment {
+public class Tab0MyRecipeFragment extends Fragment  implements IXListViewListener{
 
 //
 	private Button detailButton;
@@ -59,94 +69,99 @@ public class Tab0MyRecipeFragment extends Fragment {
 	private TextView nameTextView;
 	private int currentPageIndex;
 	private  AlertDialog dlg;
+	//瀑布流
+			private ImageFetcher mImageFetcher;
+		    private XListView mAdapterView = null;
+		    private StaggeredAdapter mAdapter = null;
+		    private int currentPage = 0;
+		    ContentTask task = new ContentTask(this.getActivity(), 2);
 	public Tab0MyRecipeFragment(JSONArray _dataArray) {
 		mdataArray=_dataArray;
-		// TODO Auto-generated constructor stub
 	}
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		Log.d("liow", "myrecipefragment");
-		
-		
-		
-		
 		mImageLoader = new ImageLoader(MyApplication.requestQueue, new BitmapCache()); 
 		
+		View view=(View)inflater.inflate(R.layout.tab0_recipe_fragment00,container,false);
+		mAdapterView = (XListView)view.findViewById(R.id.list);
+	    mAdapterView.setPullLoadEnable(true);
+	    mAdapterView.setXListViewListener(this);
+        mAdapter = new StaggeredAdapter(this.getActivity(), mAdapterView);
+        mImageFetcher = new ImageFetcher(this.getActivity(), 240);
+	    mImageFetcher.setLoadingImage(R.drawable.empty_photo);
 		
-		
-		
-		View view=inflater.inflate(R.layout.tab0_recipe_fragment,container,false);
-
-		difficultyTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment__dishdifficulty_textview);
-		doseTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment_dishamount_textview);
-		timeTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment_dishcooktime_textview);
-		nameTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment_foodname_textview);
-		skillTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment_skill_textview);
-		
-		commentbuttoButton=(Button)view.findViewById(R.id.comment_button);
-		commentbuttoButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				showDialg();
-			}
-		});
-		
-		collectionButton=(Button)view.findViewById(R.id.collection_button);
-		collectionButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
-				addCollectionRelationShip();
-				
-				
-
-			}
-		});
-		
-		
-		detailButton=(Button)view.findViewById(R.id.detail_button);
-		detailButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				
-				Intent intent=new Intent(Main.mainActivity,
-						Tab0FoodActivity.class);
-				Bundle bundle = new Bundle();                           //创建Bundle对象   
-				try {
-					bundle.putString("dishid",mdataArray.getJSONObject(currentPageIndex).getString("dishid") );
-					bundle.putString("dishname",mdataArray.getJSONObject(currentPageIndex).getString("dishname") );
-					bundle.putDouble("dishprice",mdataArray.getJSONObject(currentPageIndex).getDouble("dishprice") );
-				
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}     //装入数据   
-				intent.putExtras(bundle);                                //把Bundle塞入Intent里面  
-				startActivity(intent);
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		//
-		try {
-			initView();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		viewPager=(FoodViewPage)view.findViewById(R.id.recipe_fragment_viewpage);
-		viewPager.setAdapter(new OutsidePagerAdapter(mViews) );
-		viewPager.setOffscreenPageLimit(7);
-		foodindiactor=(CirclePageIndicator)view.findViewById(R.id.circle_page_indicator);
-		foodindiactor.setViewPager(viewPager);
-		foodindiactor.setOnPageChangeListener(new MyPageChangeAdapter1());
-	
+//	     View view=inflater.inflate(R.layout.tab0_recipe_fragment,container,false);
+//		difficultyTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment__dishdifficulty_textview);
+//		doseTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment_dishamount_textview);
+//		timeTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment_dishcooktime_textview);
+//		nameTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment_foodname_textview);
+//		skillTextView=(TextView)view.findViewById(R.id.tab0_recipe_fragment_skill_textview);
+//		
+//		commentbuttoButton=(Button)view.findViewById(R.id.comment_button);
+//		commentbuttoButton.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				showDialg();
+//			}
+//		});
+//		
+//		collectionButton=(Button)view.findViewById(R.id.collection_button);
+//		collectionButton.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				
+//				addCollectionRelationShip();
+//				
+//				
+//
+//			}
+//		});
+//		
+//		
+//		detailButton=(Button)view.findViewById(R.id.detail_button);
+//		detailButton.setOnClickListener(new OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				
+//				Intent intent=new Intent(Main.mainActivity,
+//						Tab0FoodActivity.class);
+//				Bundle bundle = new Bundle();                           //创建Bundle对象   
+//				try {
+//					bundle.putString("dishid",mdataArray.getJSONObject(currentPageIndex).getString("dishid") );
+//					bundle.putString("dishname",mdataArray.getJSONObject(currentPageIndex).getString("dishname") );
+//					bundle.putDouble("dishprice",mdataArray.getJSONObject(currentPageIndex).getDouble("dishprice") );
+//				
+//				} catch (JSONException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}     //装入数据   
+//				intent.putExtras(bundle);                                //把Bundle塞入Intent里面  
+//				startActivity(intent);
+//				// TODO Auto-generated method stub
+//				
+//			}
+//		});
+//		
+//		//
+//		try {
+//			initView();
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		viewPager=(FoodViewPage)view.findViewById(R.id.recipe_fragment_viewpage);
+//		viewPager.setAdapter(new OutsidePagerAdapter(mViews) );
+//		viewPager.setOffscreenPageLimit(7);
+//		foodindiactor=(CirclePageIndicator)view.findViewById(R.id.circle_page_indicator);
+//		foodindiactor.setViewPager(viewPager);
+//		foodindiactor.setOnPageChangeListener(new MyPageChangeAdapter1());
+//	
 		return  view;
 	}
 	
@@ -346,4 +361,207 @@ public class Tab0MyRecipeFragment extends Fragment {
 		}
 		
 		}
+	
+	 private class ContentTask extends AsyncTask<String, Integer, List<DuitangInfo>> {
+
+	        private Context mContext;
+	        private int mType = 1;
+
+	        public ContentTask(Context context, int type) {
+	            super();
+	            mContext = context;
+	            mType = type;
+	        }
+
+	        @Override
+	        protected List<DuitangInfo> doInBackground(String... params) {
+	            try {
+	                return parseNewsJSON(params[0]);
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	            return null;
+	        }
+
+	        @Override
+	        protected void onPostExecute(List<DuitangInfo> result) {
+	            if (mType == 1) {
+	                mAdapter.addItemTop(result);
+	                mAdapter.notifyDataSetChanged();
+	                mAdapterView.stopRefresh();
+
+	            } else if (mType == 2) {
+	                mAdapterView.stopLoadMore();
+	                mAdapter.addItemLast(result);
+	                mAdapter.notifyDataSetChanged();
+	            }else if(mType == 3){
+	            	  mAdapterView.stopRefresh();
+	            	  mAdapterView.stopLoadMore();
+	            	  mAdapter.notifyDataSetChanged();
+	            }
+
+	        }
+
+	        @Override
+	        protected void onPreExecute() {
+	        }
+
+	        public List<DuitangInfo> parseNewsJSON(String url) throws IOException {
+	            List<DuitangInfo> duitangs = new ArrayList<DuitangInfo>();
+	            String json = "";
+	            if (Helper.checkConnection(mContext)) {
+	                try {
+	                    json = Helper.getStringFromUrl(url);
+
+	                } catch (IOException e) {
+	                    Log.e("IOException is : ", e.toString());
+	                    e.printStackTrace();
+	                    return duitangs;
+	                }
+	            }
+	            Log.d("MainActiivty", "json:" + json);
+
+	            try {
+	                if (null != json) {
+	                    JSONObject newsObject = new JSONObject(json);
+	                	JSONArray json1 = newsObject.getJSONArray("dishsets");
+						JSONArray blogsJson = json1.getJSONObject(0).getJSONArray("dishsetdetail");
+	                    for (int i = 0; i < blogsJson.length(); i++) {
+	               JSONObject newsInfoLeftObject = blogsJson.getJSONObject(i);
+	               DuitangInfo newsInfo1 = new DuitangInfo();
+	               newsInfo1.setAlbid(newsInfoLeftObject.isNull("dishid") ? "": newsInfoLeftObject.getString("dishid"));
+				   newsInfo1.setIsrc(newsInfoLeftObject.isNull("dishpicurl") ? "": "http://110.84.129.130:8080/Yuf"+newsInfoLeftObject.getString("dishpicurl"));
+				   newsInfo1.setMsg(newsInfoLeftObject.isNull("dishname") ? "": newsInfoLeftObject.getString("dishname"));
+	               newsInfo1.setHeight(150);
+	               duitangs.add(newsInfo1);
+	                    }
+	                }
+	            } catch (JSONException e) {
+	                e.printStackTrace();
+	            }
+	            return duitangs;
+	        }
+	    }
+	 /**
+	     * 添加内容
+	     * 
+	     * @param pageindex
+	     * @param type
+	     *            1为下拉刷新 2为加载更多
+	     */
+	    private void AddItemToContainer(int pageindex, int type) {
+	    	if(currentPage==0){
+	    		 if (task.getStatus() != Status.RUNNING) {
+//	               String url = "http://www.duitang.com/album/1733789/masn/p/" + pageindex + "/24/";
+	           	String url = "http://110.84.129.130:8080/Yuf/dishset/getRecommendedDishset";
+	               Log.d("MainActivity", "current url:" + url);
+	               ContentTask task = new ContentTask(this.getActivity(), type);
+	               task.execute(url);
+	           }	
+	    	}else{
+	    		 if (task.getStatus() != Status.RUNNING) {
+//	               String url = "http://www.duitang.com/album/1733789/masn/p/" + pageindex + "/24/";
+	           	String url = "http://110.84.129.130:8080/Yuf/dishset/getRecommendedDishset";
+	               Log.d("MainActivity", "current url:" + url);
+	               ContentTask task = new ContentTask(this.getActivity(), 3);
+	               task.execute(url);
+	           }	
+	    	}
+	       
+	    }
+
+	    public class StaggeredAdapter extends BaseAdapter {
+	        private Context mContext;
+	        private LinkedList<DuitangInfo> mInfos;
+	        private XListView mListView;
+
+	        public StaggeredAdapter(Context context, XListView xListView) {
+	            this.mContext = context;
+	            this.mInfos = new LinkedList<DuitangInfo>();
+	            this.mListView = xListView;
+	        }
+
+	        @Override
+	        public View getView(int position, View convertView, ViewGroup parent) {
+
+	            ViewHolder holder;
+	            DuitangInfo duitangInfo = mInfos.get(position);
+
+	            if (convertView == null) {
+	                LayoutInflater layoutInflator = LayoutInflater.from(parent.getContext());
+	                convertView = layoutInflator.inflate(R.layout.infos_list, null);
+	                holder = new ViewHolder();
+	                holder.tab0_imageView = (ScaleImageView) convertView.findViewById(R.id.tab0_news_pic);
+	                holder.tab0_nameOfDish = (TextView) convertView.findViewById(R.id.tab0_news_title);
+	                holder.tab0_collection_button = (Button)convertView.findViewById(R.id.tab0_collection_button);
+	                holder.tab0_comment_button = (Button)convertView.findViewById(R.id.tab0_comment_button);
+	                convertView.setTag(holder);
+	            }
+
+	            holder = (ViewHolder) convertView.getTag();
+	            holder.tab0_imageView.setImageWidth(duitangInfo.getWidth());
+	            holder.tab0_imageView.setImageHeight(duitangInfo.getHeight());
+	            holder.tab0_nameOfDish.setText(duitangInfo.getMsg());
+	            mImageFetcher.loadImage(duitangInfo.getIsrc(), holder.tab0_imageView);
+	            return convertView;
+	        }
+
+	        class ViewHolder {
+	            ScaleImageView tab0_imageView;//图片
+	            TextView tab0_nameOfDish;//菜名
+	            Button tab0_collection_button;//收藏
+	            Button tab0_comment_button;//评论
+	        }
+
+	        @Override
+	        public int getCount() {
+	            return mInfos.size();
+	        }
+
+	        @Override
+	        public Object getItem(int arg0) {
+	            return mInfos.get(arg0);
+	        }
+
+	        @Override
+	        public long getItemId(int arg0) {
+	            return 0;
+	        }
+
+	        public void addItemLast(List<DuitangInfo> datas) {
+	            mInfos.addAll(datas);
+	        }
+
+	        public void addItemTop(List<DuitangInfo> datas) {
+	            for (DuitangInfo info : datas) {
+	                mInfos.addFirst(info);
+	            }
+	        }
+	    }
+	    @Override
+		public void onResume() {
+	        super.onResume();
+	        mImageFetcher.setExitTasksEarly(false);
+	        mAdapterView.setAdapter(mAdapter);
+	        AddItemToContainer(currentPage, 2);
+	    }
+
+	    @Override
+		public void onDestroy() {
+	        super.onDestroy();
+
+	    }
+
+	    @Override
+	    public void onRefresh() {
+	        AddItemToContainer(++currentPage, 1);
+
+	    }
+
+	    @Override
+	    public void onLoadMore() {
+	        AddItemToContainer(++currentPage, 2);
+
+	    }
+
 }
