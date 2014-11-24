f106cca302597d42aa65d1fc72d4ae45872cf8fc
ZM-Fight <zmhg@gmail.com>
2014-11-24 21:29:27 +0800

    唉唉

--- src/com/yuf/app/ui/Tab2Fragment.java
+++ src/com/yuf/app/ui/Tab2Fragment.java
@@ -1,16 +1,21 @@
 package com.yuf.app.ui;
 
-import com.yuf.app.mywidget.NoScrollListview;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.Button;
+
+import com.yuf.app.mywidget.NoScrollListview;
 
 public class Tab2Fragment extends Fragment {
+
 	private NoScrollListview cartListView;
+//修改收货地址	
+	private Button changeButton;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -25,6 +30,16 @@ public class Tab2Fragment extends Fragment {
 		// TODO Auto-generated method stub
 		
 		cartListView=(NoScrollListview)view.findViewById(R.id.cart_listview);
+		changeButton = (Button)view.findViewById(R.id.changeButton);
+		
+		changeButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(getActivity(), Tab2AddressEditActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
